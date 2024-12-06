117195e2b4066f42999f7ad73492b13c2e8ae99d
Matt Watson <matt@mattwatson.org>
2024-12-05 22:39:50 -0600

    Still wrong

--- day-04/part-1.php
+++ day-04/part-1.php
@@ -3,7 +3,7 @@
 // number of xmases
 $output = 0;
 
-$file_contents = file_get_contents(__DIR__.'/sample-input.txt');
+$file_contents = file_get_contents(__DIR__.'/input.txt');
 
 $rows = explode("\n", $file_contents);
 
@@ -18,12 +18,11 @@ $to_left_downs = [];
 $to_downs = [];
 
 foreach($rows as $y => $row) {
-
     foreach($row as $x => $char) {
-        $to_rights[$y][] = $char;
-        $to_downs[$x][] = $char;
-        $to_left_downs[$x + $y][] = $char;
-        $to_right_downs[$y - $x][] = $char;
+        $to_rights[$y] .= $char;
+        $to_downs[$x] .= $char;
+        $to_left_downs[$x + $y] .= $char;
+        $to_right_downs[$y - $x] .= $char;
     }
 }
 
@@ -31,17 +30,16 @@ $groups = [$to_rights, $to_downs, $to_left_downs, $to_right_downs];
 
 foreach($groups as $lines) {
     foreach($lines as $line) {
-        $str_line = implode('',$line);
-        $str_line_reverse = implode('',array_reverse($line));
-
-        if(stristr($str_line, 'XMAS')) {
+        if(stristr($line, 'XMAS')) {
             $output++;
         }
 
-        if(stristr($str_line_reverse, 'XMAS')) {
+        if(stristr(strrev($line), 'XMAS')) {
             $output++;
         }
     }
 }
+
+var_dump($to_right_downs);
 
 var_dump($output);
