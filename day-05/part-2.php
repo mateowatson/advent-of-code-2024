92792a2bd3a74b17806b19d9aefd67ea8c225764
Matt Watson <matt@mattwatson.org>
2024-12-07 13:53:25 -0600

    Wrong answer

--- day-05/part-2.php
+++ day-05/part-2.php
@@ -3,7 +3,7 @@
 // sum of middle numbers of correct reports
 $output = 0;
 
-$file_contents = file_get_contents(__DIR__.'/sample-input.txt');
+$file_contents = file_get_contents(__DIR__.'/input.txt');
 
 $rows = explode("\n", $file_contents);
 
@@ -27,32 +27,21 @@ foreach($updates as $ui => $update) {
     $is_correct = true;
     
     foreach($rules as $ri => $rule) {
-        $first = array_search($rule[0], $update);
-        $second = array_search($rule[1], $update);
+        $first = array_search($rule[0], $updates[$ui]);
+        $second = array_search($rule[1], $updates[$ui]);
 
         if($first === false || $second === false) continue;
 
         if($first > $second) {
             $is_correct = false;
+            $second_num = array_splice($updates[$ui], $second, 1)[0];
+            array_splice($updates[$ui], $first, 0, $second_num);
         }
     }
 
     if(!$is_correct) {
-        foreach($rules as $ri => $rule) {
-            $first = array_search($rule[0], $updates[$ui]);
-            $second = array_search($rule[1], $updates[$ui]);
-
-            if($first === false || $second === false) continue;
-
-            if($first > $second) {
-                $second_num = array_splice($updates[$ui], $second, 1)[0];
-                array_splice($updates[$ui], $first, 0, $second_num);
-            }
-        }
-
-        var_dump(implode(',',$updates[$ui]));
         $output += $updates[$ui][floor(count($update)/2)];
     }
 }
 
-var_dump($output);
+var_dump($output); // 3894 too low
