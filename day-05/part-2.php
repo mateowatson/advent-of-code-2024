a7930513796993334e5439e89b3196ffb9b31e52
Matt Watson <matt@mattwatson.org>
2024-12-07 13:50:01 -0600

    Correct answer with sample data

--- day-05/part-2.php
+++ day-05/part-2.php
@@ -27,40 +27,32 @@ foreach($updates as $ui => $update) {
     $is_correct = true;
     
     foreach($rules as $ri => $rule) {
-        $first = array_search($rule[0], $updates[$ui]);
-        $second = array_search($rule[1], $updates[$ui]);
+        $first = array_search($rule[0], $update);
+        $second = array_search($rule[1], $update);
 
         if($first === false || $second === false) continue;
 
         if($first > $second) {
             $is_correct = false;
-            $first_num = $updates[$ui][$first];
-            $second_num = $updates[$ui][$second];
-            // remove num that should be second
-            array_splice($updates[$ui], $second, 1);
-            // put it right after num that should be first
-            array_splice($updates[$ui], $first+1, 0, $second_num);
         }
     }
 
     if(!$is_correct) {
+        foreach($rules as $ri => $rule) {
+            $first = array_search($rule[0], $updates[$ui]);
+            $second = array_search($rule[1], $updates[$ui]);
+
+            if($first === false || $second === false) continue;
+
+            if($first > $second) {
+                $second_num = array_splice($updates[$ui], $second, 1)[0];
+                array_splice($updates[$ui], $first, 0, $second_num);
+            }
+        }
+
         var_dump(implode(',',$updates[$ui]));
-        $output += $updates[$ui][floor(count($updates[$ui])/2)];
+        $output += $updates[$ui][floor(count($update)/2)];
     }
 }
 
-// $updates = array_values(array_filter($updates, fn($update) => $update !== ''));
-
-// foreach($updates as $ui => $update) {
-
-// }
-
 var_dump($output);
-
-$arr = [97,13,75,29,47];
-// remove num that should be first
-array_splice($arr, 1, 1);
-// put it right before num that should be second
-array_splice($arr, 0, 0, 13);
-
-// var_dump($arr);
