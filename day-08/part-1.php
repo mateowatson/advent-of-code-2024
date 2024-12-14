c65561caf1f76673fabe688e69e879496388f1c2
Matt Watson <matt@mattwatson.org>
2024-12-14 11:07:31 -0600

    Finished day 8 part 1

--- day-08/part-1.php
+++ day-08/part-1.php
@@ -28,10 +28,6 @@ foreach($antennas as $ai1 => $antenna1) {
     foreach($antennas as $ai2 => $antenna2) {
         if($ai2 <= $ai1) continue; 
         if($antenna1['char'] !== $antenna2['char']) continue;
-        // $antinode1 = [
-        //     $antenna1['x'] + abs($antenna2['x'] - $antenna1['x']),
-        //     $antenna1['y'] - abs($antenna2['y'] - $antenna1['y'])
-        // ];
 
         $antinode1 = [
             $antenna1['x'] + $antenna1['x'] - $antenna2['x'],
@@ -43,23 +39,9 @@ foreach($antennas as $ai1 => $antenna1) {
             $antenna2['y'] + $antenna2['y'] - $antenna1['y']
         ];
 
-        /**
-         * antinode1 [3, 1]
-         * a [4, 3]
-         * b [5, 5]
-         * antinode2 [6, 7]
-         * 
-         * antinode 1 [10, 2]
-         * a [7, 3]
-         * b [4, 4]
-         * antinode 2 [1, 5]
-         */
-
         $antinode1val = get_point($antinode1[0],$antinode1[1]);
 
         $antinode2val = get_point($antinode2[0],$antinode2[1]);
-
-        // var_dump('-------',$antinode1, $antinode1val, $antinode2, $antinode2val);
 
         if($antinode1val !== null) {
             $antinode_points[] = implode(',',$antinode1);
@@ -71,7 +53,7 @@ foreach($antennas as $ai1 => $antenna1) {
     }
 }
 
-var_dump(count($antinode_points));
+var_dump(count(array_unique($antinode_points)));
 
 function get_point($x, $y) {
     global $rows;
