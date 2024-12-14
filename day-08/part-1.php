7b3d6a989f5d590f5a95df9e4eb847721aaa41b9
Matt Watson <matt@mattwatson.org>
2024-12-14 11:02:13 -0600

    Correct for sample input but not real input

--- day-08/part-1.php
+++ day-08/part-1.php
@@ -1,6 +1,6 @@
 <?php
 
-$file_contents = file_get_contents(__DIR__.'/sample-input.txt');
+$file_contents = file_get_contents(__DIR__.'/input.txt');
 
 $rows = explode("\n", $file_contents);
 
@@ -28,15 +28,50 @@ foreach($antennas as $ai1 => $antenna1) {
     foreach($antennas as $ai2 => $antenna2) {
         if($ai2 <= $ai1) continue; 
         if($antenna1['char'] !== $antenna2['char']) continue;
-        $antinode1 = [
-            abs($antenna2['x'] - $antenna1['x']),
-            abs($antenna2['y'] - $antenna1['y'])
-        ];
-        $antinode1val = get_point($antinode1['x'],$antinode1['y']);
+        // $antinode1 = [
+        //     $antenna1['x'] + abs($antenna2['x'] - $antenna1['x']),
+        //     $antenna1['y'] - abs($antenna2['y'] - $antenna1['y'])
+        // ];
 
-        var_dump('-------',$antinode1,$antinode1val,'======',$antenna1,$antenna2);
+        $antinode1 = [
+            $antenna1['x'] + $antenna1['x'] - $antenna2['x'],
+            $antenna1['y'] + $antenna1['y'] - $antenna2['y']
+        ];
+
+        $antinode2 = [
+            $antenna2['x'] + $antenna2['x'] - $antenna1['x'],
+            $antenna2['y'] + $antenna2['y'] - $antenna1['y']
+        ];
+
+        /**
+         * antinode1 [3, 1]
+         * a [4, 3]
+         * b [5, 5]
+         * antinode2 [6, 7]
+         * 
+         * antinode 1 [10, 2]
+         * a [7, 3]
+         * b [4, 4]
+         * antinode 2 [1, 5]
+         */
+
+        $antinode1val = get_point($antinode1[0],$antinode1[1]);
+
+        $antinode2val = get_point($antinode2[0],$antinode2[1]);
+
+        // var_dump('-------',$antinode1, $antinode1val, $antinode2, $antinode2val);
+
+        if($antinode1val !== null) {
+            $antinode_points[] = implode(',',$antinode1);
+        }
+
+        if($antinode2val !== null) {
+            $antinode_points[] = implode(',',$antinode2);
+        }
     }
 }
+
+var_dump(count($antinode_points));
 
 function get_point($x, $y) {
     global $rows;
